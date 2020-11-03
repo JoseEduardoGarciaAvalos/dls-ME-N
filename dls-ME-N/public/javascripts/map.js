6b22863df33b557b226c77fae6b487110efc03e0
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-02 20:55:06 -0600

    API REST básico

--- dls-ME-N/public/javascripts/map.js
+++ dls-ME-N/public/javascripts/map.js
@@ -3,7 +3,13 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(mymap);
 
-
-L.marker([-34.6012424, -58.3861497]).addTo(mymap);
-L.marker([-34.6102524, -58.3871597]).addTo(mymap);
-L.marker([-34.6062624, -58.3881697]).addTo(mymap);
+$.ajax({
+    datType: "json",
+    url: "api/bicicletas",
+    success: function(res) {
+        console.log(res);
+        res.bicicletas.forEach( function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(mymap);
+        })
+    }
+})
