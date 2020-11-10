8e2f321b3fa0486ebe954dfe717745773926198f
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-10 14:38:33 -0600

    3.5 Vistas login, recupero de password,etc y algunas arreglos

--- app.js
+++ app.js
@@ -31,13 +31,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/login', function (req, res) {
+  res.render('session/login');
+});
+
+app.post('/login', function (req, res, next) {
+  // passport
+});
+
+app.get('/logout', function (req, res) {
+  req.logOut();
+  res.redirect('/');
+});
+
+app.get('/forgotPassword', function (req, res) {
+  res.render('session/forgotPassword');
+});
+
+app.post('/forgotPassword', function (req, res, next) {
+  
+});
+
 app.use('/usuarios', usuariosRouter);
 app.use('/token', tokenRouter);
+
 app.use('/bicicletas', bicicletasRouter);
 app.use('/api/bicicletas', bicicletasAPIRouter);
 app.use('/api/usuarios', usuariosAPIRouter);
+
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
