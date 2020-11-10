d5cc34ce98e173d6485599ba3b909883f083946c
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-10 11:41:13 -0600

    3.1 atributos del modelo usuario con restricciones

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,51 @@
 var mongoose = require('mongoose');
 var Reserva = require('./reserva');
 var Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
 
 var usuarioSchema = new Schema({
-  nombre: String,
+  nombre: {
+    type: String,
+    trim: true,
+    required: [true, 'El nombre es obligatorio']
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: [true, 'El email es obligatorio'],
+    lowercase: true,
+    unique: true,
+    validate: [validateEmail, 'Por favor ingrese un email válido'],
+    match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/]
+  },
+  password: {
+    type: String,
+    required: [true, 'El password es obligatorio']
+  },
+  passwordResetToken: String,
+  passwordResetTokenExpires: Date,
+  verificado: {
+    type: Boolean,
+    default: false
+  },
 });
+
+usuarioSchema.pre('save', function (next) {
+  if (this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+  }
+  next();
+});
+
+usuarioSchema.methods.validPassword = function (password) { 
+  return bcrypt.compareSync(password, this.password);
+}
 
 usuarioSchema.methods.reservar = function (biciId, desde, hasta, cb) {
   var reserva = new Reserva({ usuario: this._id, bicicleta: biciId, desde: desde, hasta: hasta });
