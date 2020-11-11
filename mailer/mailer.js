a1978271a77b23795d56d214ddd12dd3bcfab612
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-11 10:03:14 -0600

    4.6-7 Uso de Sendgrid, y Ethereal en el ambiente local

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,34 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-  host: 'smtp.ethereal.email',
-  port: 587,
-  auth: {
-    user: "jolie.klein17@ethereal.email",
-    pass: "xuySgMquAdWkdkkYDx"
+let mailConfig;
+if (process.env.NODE_ENV === 'production') {
+  const options = {
+    auth: {
+      api_key: process.env.SENDGRID_API_SECRET
+    }
   }
-};
+  mailConfig = sgTransport(options);
+} else {
+  if (process.env.NODE_ENV === 'staging') {
+    console.log('xxxxxxxxxxxxxxx');
+    const options = {
+      auth: {
+        api_key: process.env.SENDGRID_API_SECRET
+      }
+    }
+    mailConfig = sgTransport(options);
+  }
+  else {
+    mailConfig = {
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: process.env.ethereal_user,
+        pass: process.env.ethereal_pwd 
+      }
+    };
+  }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
